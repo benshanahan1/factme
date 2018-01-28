65aead159c4cde8283f767a3451ca465f48c6c5f
Ben <benshanahan1@gmail.com>
2018-01-27 19:26:45 -0500

    added database

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ from flask import Flask
 from config.config import IP, PORT
 from json import dumps, loads
 from flask import Flask, request, abort
-from flask_restful import Resource, Api
+from flask_restful import Api
 from flask_cors import CORS
-from lib.database import Database
+from lib.api import Endpoint
 from gevent.wsgi import WSGIServer
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
@@ -17,5 +19,7 @@ def index():
 
 
 if __name__ == "__main__":
+    api = Api(app)
+    api.add_resource(Endpoint, "/v1/<path:key_path>")
     http_server = WSGIServer((IP, PORT), app)
     http_server.serve_forever()
