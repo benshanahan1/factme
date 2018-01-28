427d0d84da75bffa41c03982c65ff3a3bb6af42e
Izzy Brand <isaiah_brand@brown.edu>
2018-01-28 05:54:22 -0500

    added cookies for keeping track of users

--- factme-extension/background.js
+++ factme-extension/background.js
@@ -2,6 +2,38 @@ var apiURL = "http://138.16.49.17/v1/";
 var userID = "234567";
 
 
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+
+  guid =  s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+    endpoint ='create_user/' + guid;
+    api_post(endpoint=endpoint, payload={}, response =function(data) {
+        if (!data) {
+          alert("Failed to create user. Reload this page.")
+        }
+    })
+  return guid;  
+
+}
+
+cookieValue = undefined;
+chrome.cookies.get({url:"http://fact.me",name:"fact.me"}, function(cookie) {
+  if (cookie == null) {
+    chrome.cookies.set({url:"http://fact.me",name:"fact.me", expirationDate:2000000000, value:guid()}, function(cookie) {console.log(cookie)})
+  }
+})
+
+
+chrome.cookies.get({url:"http://fact.me",name:"fact.me"}, function(cookie) {
+  userID = cookie.value;
+  console.log(cookie.value)}
+  );
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     
